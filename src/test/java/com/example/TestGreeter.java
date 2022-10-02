2d1644a7477b93b2111b678f9ee2bd7eb4c95a83
sanjeet kumar <sanjeetcalgary@gmail.com>
2022-10-02 10:11:23 -0600

    method name changed

--- src/test/java/com/example/TestGreeter.java
+++ src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
   @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessageNew() {
     String someone = "World, Thanks you";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
