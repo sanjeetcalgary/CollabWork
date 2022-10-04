85b81a14e1f7a73894dfd0fc29f290d6903b5a8d
sanjeet kumar <sanjeetcalgary@gmail.com>
2022-10-03 20:00:33 -0600

    dsl added

---
+++ Simple-JavaMavenProject/job-DSL.groovy
@@ -0,0 +1,23 @@
+job('DSL-job') {
+    description("This is my first DSL job at ${new Date()}")
+
+    scm{
+        git("https://github.com/sanjeetcalgary/CollabWork.git",'main')
+    }
+
+    trigger{
+        scm('* * * * *')
+    }
+
+    steps{
+        maven('clean install test package','Simple-JavaMavenProject/pom.xml')
+    }
+
+    publishers{
+        archiveJunit('Simple-JavaMavenProject/target/surefire-reports/*.xml')
+    }
+
+    publishers{
+        archiveArtifacts '**/*.jar'
+    }
+}
